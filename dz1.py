4c2623026ef9b23419cab257aa539b39af172741
HARB1I <156460883+HARB1I@users.noreply.github.com>
2024-03-02 08:34:27 +0400

    Add files via upload

--- dz1.py
+++ dz1.py
@@ -9,22 +9,30 @@ nums = {'0':"ноль",'1':'один','2':"два",
 k = input("введите число k: ")
 k = int(k)
 max_num = 4096
+buf_len = 1
 max_num_oct = int(oct(max_num)[2:])
-num = []
 num_list = []
-file_nums = []
+file_nums = []                                                             
+work_buffer = ""
 with open("text.txt","r") as file:
-    file_nums.append(file.read().split())
-    for i in range(len(file_nums[0])):
-        num.append(file_nums[0][i])
-    for n in range(len(num)):
-        if abs(int(num[n])) <= max_num_oct and abs(int(num[n])) % 2 == 1 and len(str(num[n])) % 2 == 0 and len(str(num[n])) > k:
-                for x in str(num[n]):
+      buffer = file.read(buf_len)                                    
+      while buffer:                                                       
+           while buffer >= '0' and buffer <= '7':                                                            
+              work_buffer += buffer
+              buffer = file.read(buf_len)
+           if len(work_buffer) > 0:
+             if abs(int(work_buffer)) <= max_num_oct and abs(int(work_buffer)) % 2 == 1 and len(str(work_buffer)) % 2 == 0 and len(str(work_buffer)) > k:
+                for x in str(work_buffer): 
                     num_list.append(int(x))
                 for o in num_list:
                     print(nums[str(o)], end = " ")
                 print(" ")
                 num_list = []
+           work_buffer = ''
+           buffer = file.read(buf_len)
+              
+                
+      
                                     
 
         
